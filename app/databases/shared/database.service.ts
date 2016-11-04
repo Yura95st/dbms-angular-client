b2e4f14e782e539f47feeafe6b1afa7727148947
yura95st <yura95st@gmail.com>
2016-11-04 22:36:37 +0200

    Move apiUrl to Config class.

--- app/databases/shared/database.service.ts
+++ app/databases/shared/database.service.ts
@@ -4,14 +4,12 @@ import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+import { Config } from './config';
 import { Database } from './database.model';
 import { Table } from './table.model';
 
 @Injectable()
 export class DatabaseService {
-    private static baseUrl: string = 'http://dbmsapp.azurewebsites.net/api';
-    // private static baseUrl: string = 'http://localhost:62227/api';
-
     private headers: Headers;
 
     constructor(private http: Http) {
@@ -20,22 +18,22 @@ export class DatabaseService {
     }
 
     getDbNames(): Observable<string[]> {
-        return this.http.get(`${DatabaseService.baseUrl}/databases`, { headers: this.headers })
+        return this.http.get(`${Config.apiUrl}/databases`, { headers: this.headers })
             .map(response => response.json());
     }
 
     getDatabase(dbName: string): Observable<Database> {
-        return this.http.get(`${DatabaseService.baseUrl}/databases/${dbName}`, { headers: this.headers })
+        return this.http.get(`${Config.apiUrl}/databases/${dbName}`, { headers: this.headers })
             .map(response => response.json());
     }
 
     getTable(dbName: string, tableName: string): Observable<Table> {
-        return this.http.get(`${DatabaseService.baseUrl}/databases/${dbName}/${tableName}`, { headers: this.headers })
+        return this.http.get(`${Config.apiUrl}/databases/${dbName}/${tableName}`, { headers: this.headers })
             .map(response => response.json());
     }
 
     getTableProjection(dbName: string, tableName: string, attributesNames: string[]): Observable<Table> {
-        let queryUrl: string = `${DatabaseService.baseUrl}/databases/${dbName}/${tableName}/projection`;
+        let queryUrl: string = `${Config.apiUrl}/databases/${dbName}/${tableName}/projection`;
 
         let isFirst: boolean = true;
         for (let attributesName of attributesNames) {
